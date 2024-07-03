266f66713d7120c8d94cf37ce4f7301b4589dc91
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2024-07-02 22:52:22 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "tiny-basic";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      devShells.${system}.default =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            clippy
+            rustc
+            rustfmt
+          ];
+        };
+    };
+}
